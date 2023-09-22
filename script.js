98f2cca348149aa0f12c12878d56d9253c51c990
Mayur Hanwate <70631103+lordbakyarou@users.noreply.github.com>
2023-09-22 23:50:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+
+	let numa = "";
+
+
+	while(num != 0){
+		numa += num %2;
+		num  = num /2;
+	}
+	return numa;
 }
 
 window.decimalToBinary = decimalToBinary;
